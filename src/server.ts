dc5fc480813b397309a483ac40c56fbcb8683e0a
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-28 23:56:59 +0600

    initial setup done and server and app created

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+
+let server: Server;
+
+
+const startServer = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://mongodb:mongodb@cluster0.d3h8n.mongodb.net/digitalWallet?retryWrites=true&w=majority&appName=Cluster0"
+    );
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+
+    server = app.listen(5000, () => {
+      console.log(`⚡ Server is running on port: 5000.`);
+    });
+  } catch (error) {
+    console.log(`❌ Error: ${error}`);
+    process.exit(1);
+  }
+};
+
+startServer();
+
