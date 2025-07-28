5b4f45b6ee1af5308dfeb2183606ffbb0a45fec0
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-29 00:44:59 +0600

    eslint setup done

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
-    message: "⚡ Welcome to Digital Wallet server..",
+    message: "⚡ Welcome to Digital Wallet Server..",
   });
 });
 
