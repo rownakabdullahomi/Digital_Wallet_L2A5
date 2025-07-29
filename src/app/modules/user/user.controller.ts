12b0d22eed8b414a172f74b34d9416af48af931e
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-07-29 22:05:48 +0600

    catch async function added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,24 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 import { UserService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
+// const createUser = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//         const user = await UserService.createUser(req.body)
 
+//         res.status(httpStatus.CREATED).json({
+//             success: true,
+//             message: `✅ User created successfully.`,
+//             user,
+//         })
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const user = await UserService.createUser(req.body)
+//     } catch (error) {
+//         next(error);
+//     }
+// }
 
-        res.status(httpStatus.CREATED).json({
-            success: true,
-            message: `✅ User created successfully.`,
-            user,
-        })
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const user = await UserService.createUser(req.body);
+    res.status(httpStatus.CREATED).json({
+      success: true,
+      message: `✅ User created successfully.`,
+      user,
+    });
+  }
+);
 
-    } catch (error) {
-        next(error);
-    }
-}
+const getAllUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserService.getAllUsers();
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: `✅ All users retrieved successfully.`,
+      users,
+    });
+});
 
 export const UserController = {
-    createUser
-}
+  createUser,
+  getAllUsers,
+};
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -46,6 +46,12 @@ const createUser = async (payload: Partial<IUser>) => {
   return user;
 };
 
+const getAllUsers = async()=>{
+  const users = await User.find();
+  return users;
+}
+
 export const UserService = {
   createUser,
+  getAllUsers,
 };
